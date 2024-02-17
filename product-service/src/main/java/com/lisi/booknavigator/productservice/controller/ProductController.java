ef124ed9a56deba530c6210215571edb6dc0a3a8
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-02-17 13:34:25 +0000

    add product service

--- product-service/src/main/java/com/lisi/booknavigator/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/lisi/booknavigator/productservice/controller/ProductController.java
@@ -1,16 +1,21 @@
 package com.lisi.booknavigator.productservice.controller;
 
 import com.lisi.booknavigator.productservice.dto.ProductRequest;
+import com.lisi.booknavigator.productservice.service.ProductService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstrucctor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-
+        productService.createProduct(productRequest);
     }
 }
--- product-service/src/main/java/com/lisi/booknavigator/productservice/service/ProductService.java
+++ product-service/src/main/java/com/lisi/booknavigator/productservice/service/ProductService.java
@@ -2,16 +2,24 @@ package com.lisi.booknavigator.productservice.service;
 
 import com.lisi.booknavigator.productservice.dto.ProductRequest;
 import com.lisi.booknavigator.productservice.model.Product;
+import com.lisi.booknavigator.productservice.repository.ProductRepository;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstrucctor
 public class ProductService {
+
+    private final ProductRepository productRespProductRepository;
+
     public void createProduct(ProductRequest productRequest){
         Product product = Product.builder()
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build();
-
+        
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
     }
 }
