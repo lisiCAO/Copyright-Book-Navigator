8feeb620b6278db7cae1128b66082f5fd88b3dee
曹励斯 <caolisi@caolisideimac.home>
2024-02-20 22:58:13 -0500

    inventory and order service communication

---
+++ microservices-project/inventory-service/src/main/java/com/lisi/booknavigator/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,32 @@
+package com.lisi.booknavigator.inventoryservice;
+
+import com.lisi.booknavigator.inventoryservice.model.Inventory;
+import com.lisi.booknavigator.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args ->{
+			Inventory inventory =new Inventory();
+			inventory.setSkuCode("iphone_15");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_15_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+}
