4abbf26a8c155addcf917101c820572ff6c4205a
曹励斯 <caolisi@caolisideimac.home>
2024-02-20 23:14:27 -0500

    keep docs onlly

--- microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/reponsitory/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lisi.booknavigator.orderservice.reponsitory;
-
-import com.lisi.booknavigator.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservices-project/product-service/src/main/java/com/lisi/booknavigator/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lisi.booknavigator.productservice.repository;
-
-import com.lisi.booknavigator.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
