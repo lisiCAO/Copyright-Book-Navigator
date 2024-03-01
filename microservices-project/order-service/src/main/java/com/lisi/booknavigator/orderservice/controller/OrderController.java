484bf9d35dad18dea6420cd36361f0895eb3e181
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-03-01 13:51:34 -0500

    Feature/circuit breaker (#4)
    * TODO:migrate to micrometer
    
    * update to spring 3 sleuth
    
    * create custom span

--- microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/controller/OrderController.java
+++ microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/controller/OrderController.java
@@ -2,21 +2,36 @@ package com.lisi.booknavigator.orderservice.controller;
 
 import com.lisi.booknavigator.orderservice.dto.OrderRequest;
 import com.lisi.booknavigator.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some times!") ;
     }
 }
