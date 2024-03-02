a35adcbc952d4c3e7b6346fe5fd9283922b07fd1
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-03-02 07:59:28 -0500

    Feature/circuit breaker (#5)
    * TODO:migrate to micrometer
    
    * update to spring 3 sleuth
    
    * create custom span
    
    * temp kafka

--- microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/service/OrderService.java
+++ microservices-project/order-service/src/main/java/com/lisi/booknavigator/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package com.lisi.booknavigator.orderservice.service;
 import com.lisi.booknavigator.orderservice.dto.InventoryResponse;
 import com.lisi.booknavigator.orderservice.dto.OrderLineItemsDto;
 import com.lisi.booknavigator.orderservice.dto.OrderRequest;
+import com.lisi.booknavigator.orderservice.event.OrderPlacedEvent;
 import com.lisi.booknavigator.orderservice.model.Order;
 import com.lisi.booknavigator.orderservice.model.OrderLineItems;
 import com.lisi.booknavigator.orderservice.reponsitory.OrderRepository;
@@ -10,6 +11,7 @@ import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -26,6 +28,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -60,6 +63,7 @@ public class OrderService {
 
             if (AllProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 log.info("Order {} is saved", order.getId());
                 return "Order Placed Successfully";
             } else {
