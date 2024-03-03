eafb2e0620ccac93a1dfa624c5521f5b6c19abfa
lisiCAO <caolisi@hotmail.com>
2024-03-03 10:31:22 -0500

    finish

--- microservices-project/api-gateway/src/main/java/com/lisi/booknavigator/apigateway/config/SecurityConfig.java
+++ microservices-project/api-gateway/src/main/java/com/lisi/booknavigator/apigateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.lisi.booknavigator.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
